3de7e347a7d6a58ce93e42917bb3d579403674e0
Toshbrown <toshbrown@gmail.com>
2016-12-05 11:12:57 +0000

    Added HTTPS  and macaroon support

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 var Promise = require('promise');
-var request = require('request');
+var databoxRequest = require('./lib/databox-request.js');
+var databoxDatasourceHelper = require('./lib/databox-datasource-helper.js');
+
 var monitor = require("os-monitor");
 var databox_directory = require("./utils/databox_directory.js");
 
@@ -36,96 +38,50 @@ app.listen(8080, function () {
 var DATASTORE_TIMESERIES_ENDPOINT = process.env.DATABOX_OS_MONITOR_DRIVER_DATASTORE_TIMESERIES_ENDPOINT;
 
 
-var sensors = ['loadavg1','loadavg5','loadavg15','freemem'];
-var SENSOR_TYPE_IDs = [];
-var SENSOR_IDs = {};
+var vendor = "databox";
 
-var VENDOR_ID = null;
-var DRIVER_ID = null;
-var DATASTORE_ID = null;
-
-
-databox_directory.register('databox','databox-os-monitor-driver','Cool OS monitoring driver')
-  .then((ids) => {
-    console.log(ids);
-    VENDOR_ID = ids['vendor_id'];
-    DRIVER_ID = ids['driver_id'];
-    
-    console.log("VENDOR_ID", VENDOR_ID);
-    console.log("DRIVER_ID", DRIVER_ID);
-
-    return databox_directory.get_datastore_id('databox-os-monitor-driver-datastore-timeseries');
-  })
-  .then ((datastore_id) => {
-    DATASTORE_ID = datastore_id;
-    console.log("DATASTORE_ID", DATASTORE_ID);
+databoxDatasourceHelper.waitForDatastore(DATASTORE_TIMESERIES_ENDPOINT)
+  .then(() =>{
     proms = [
-      databox_directory.register_sensor_type('loadavg1'),
-      databox_directory.register_sensor_type('loadavg5'),
-      databox_directory.register_sensor_type('loadavg15'),
-      databox_directory.register_sensor_type('freemem')
+      databoxDatasourceHelper.registerDatasource(DATASTORE_TIMESERIES_ENDPOINT, vendor, 'loadavg1', 'loadavg1', 'percent', '', 'System load over 1 minuet', 'In the databox'),
+      databoxDatasourceHelper.registerDatasource(DATASTORE_TIMESERIES_ENDPOINT, vendor, 'loadavg5', 'loadavg5', 'percent', '', 'System load over 5 minuet', 'In the databox'),
+      databoxDatasourceHelper.registerDatasource(DATASTORE_TIMESERIES_ENDPOINT, vendor, 'loadavg15', 'loadavg15', 'percent', '', 'System load over 15 minuet', 'In the databox'),
+      databoxDatasourceHelper.registerDatasource(DATASTORE_TIMESERIES_ENDPOINT, vendor, 'freemem', 'freemem', 'bytes', 'b', 'Free memory', 'In the databox'),
     ]
     return Promise.all(proms);
   })
-  .then ((sensorTypeIds) => {
-    console.log('sensorTypeIds::', sensorTypeIds);
-    SENSOR_TYPE_IDs = sensorTypeIds;
-    proms = [
-      databox_directory.register_sensor(DRIVER_ID, SENSOR_TYPE_IDs[0].id, DATASTORE_ID, VENDOR_ID, 'loadavg1', 'percent', '', 'System load over 1 minuet', 'In the databox'),
-      databox_directory.register_sensor(DRIVER_ID, SENSOR_TYPE_IDs[1].id, DATASTORE_ID, VENDOR_ID, 'loadavg5', 'percent', '', 'System load over 5 minuet', 'In the databox'),
-      databox_directory.register_sensor(DRIVER_ID, SENSOR_TYPE_IDs[2].id, DATASTORE_ID, VENDOR_ID, 'loadavg15', 'percent', '', 'System load over 15 minuet', 'In the databox'),
-      databox_directory.register_sensor(DRIVER_ID, SENSOR_TYPE_IDs[3].id, DATASTORE_ID, VENDOR_ID, 'freemem', 'bytes', 'b', 'Free memory', 'In the databox'),
-    ]
-    return Promise.all(proms);
-  })
-  .then((sensorIds) => {
-    console.log("sensorIds::", sensorIds); 
-    for(var i = 0; i < SENSOR_TYPE_IDs.length; i++) {
-      SENSOR_IDs[sensors[i]] = sensorIds[i].id;
-    }
+  .then(() => {
 
-    console.log("SENSOR_IDs", SENSOR_IDs);
+      function saveReading(name,reading) {
+        var options = {
+            uri: DATASTORE_TIMESERIES_ENDPOINT + '/reading',
+            method: 'POST',
+            json: 
+            {
+              sensor_id: name, 
+              vendor_id: vendor, 
+              value: reading   
+            }
+        };
+        databoxRequest(options, (error, response, body) => {console.log(error, body)});
+      }
 
-    function saveReading(name,reading) {
-      var options = {
-          uri: DATASTORE_TIMESERIES_ENDPOINT + '/reading',
-          method: 'POST',
-          json: 
-          {
-            sensor_id: SENSOR_IDs[name], 
-            vendor_id: VENDOR_ID, 
-            value: reading   
-          }
-      };
-      request.post(options, (error, response, body) => {console.log(error, body)});
-    }
+      monitor.start({ delay: 5000 });
+        // define handler that will always fire every cycle
+        monitor.on('monitor', function(event) {
+          
+          var loadavg1 = event['loadavg'][0];
+          var loadavg5 = event['loadavg'][1];
+          var loadavg15 = event['loadavg'][2];
+          var freemem = event[freemem];
+          console.log(loadavg1);
 
-
-    monitor.start({ delay: 5000 });
-
-    // define handler that will always fire every cycle
-    monitor.on('monitor', function(event) {
-      
-      var loadavg1 = event['loadavg'][0];
-      var loadavg5 = event['loadavg'][1];
-      var loadavg15 = event['loadavg'][2];
-      var freemem = event[freemem];
-      console.log(loadavg1);
-
-      saveReading('loadavg1', event['loadavg'][0]);
-      saveReading('loadavg5', event['loadavg'][1]);
-      saveReading('loadavg15', event['loadavg'][2]);
-      saveReading('freemem', event['freemem']);
-
-    });
+          saveReading('loadavg1', event['loadavg'][0]);
+          saveReading('loadavg5', event['loadavg'][1]);
+          saveReading('loadavg15', event['loadavg'][2]);
+          saveReading('freemem', event['freemem']);
+      });
   })
   .catch((err) => {
     console.log(err);
-  })
-
-
-
-
-
-
-
+  });
