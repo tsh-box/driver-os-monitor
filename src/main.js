2f8de1704174f94ec73c458c92ad74c6b7a23d5d
toshbrown <Toshbrown@gmail.com>
2017-12-08 09:27:32 +0000

    Update to work with core-store (#6)
    * Update to work with core-store
    
    * Update docker file node-lib-databox and version

--- src/main.js
+++ src/main.js
@@ -9,9 +9,10 @@ const monitor = require("os-monitor");
 const databox = require('node-databox');
 
 //
-// Get the needed Environment variables 
+// Get the needed Environment variables
 //
 const DATABOX_STORE_BLOB_ENDPOINT = process.env.DATABOX_STORE_ENDPOINT;
+const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT
 
 //HTTPS certs created by the container mangers for this components HTTPS server.
 credentials = databox.getHttpsCredentials();
@@ -29,88 +30,89 @@ app.get("/status", function(req, res) {
 
 var vendor = "databox inc";
 
-databox.waitForStoreStatus(DATABOX_STORE_BLOB_ENDPOINT,'active',10)
-  .then(() => {
-    
-    proms = [
-      databox.catalog.registerDatasource(DATABOX_STORE_BLOB_ENDPOINT, {
-        description: 'Databox load average 1 minuet',
-        contentType: 'text/json',
-        vendor: 'Databox Inc.',
-        unit:'%',
-        type: 'loadavg1',
-        datasourceid: 'loadavg1',
-        storeType: 'store-json'
-      }),
+let tsc = databox.NewTimeSeriesClient(DATABOX_ZMQ_ENDPOINT, false);
 
-      databox.catalog.registerDatasource(DATABOX_STORE_BLOB_ENDPOINT, {
-        description: 'Databox load average 5 minuets',
-        contentType: 'text/json',
-        vendor: 'Databox Inc.',
-        unit:'%',
-        type: 'loadavg5',
-        datasourceid: 'loadavg5',
-        storeType: 'store-json'
-      }),
+let loadavg1 = databox.NewDataSourceMetadata();
+loadavg1.Description = 'Databox load average 1 minute';
+loadavg1.ContentType = 'application/json';
+loadavg1.Vendor = 'Databox Inc.';
+loadavg1.Unit = '%';
+loadavg1.DataSourceType = 'loadavg1';
+loadavg1.DataSourceID = 'loadavg1';
+loadavg1.StoreType = 'ts';
 
-      databox.catalog.registerDatasource(DATABOX_STORE_BLOB_ENDPOINT, {
-        description: 'Databox load average 15 minuets',
-        contentType: 'text/json',
-        vendor: 'Databox Inc.',
-        unit:'%',
-        type: 'loadavg15',
-        datasourceid: 'loadavg15',
-        storeType: 'store-json'
-      }),
+let loadavg5 = databox.NewDataSourceMetadata();
+loadavg5.Description = 'Databox load average over 5 minutes';
+loadavg5.ContentType = 'application/json';
+loadavg5.Vendor = 'Databox Inc.';
+loadavg5.Unit = '%';
+loadavg5.DataSourceType = 'loadavg5';
+loadavg5.DataSourceID = 'loadavg5';
+loadavg5.StoreType = 'ts';
 
-       databox.catalog.registerDatasource(DATABOX_STORE_BLOB_ENDPOINT, {
-        description: 'Free memory in bytes',
-        contentType: 'text/json',
-        vendor: 'Databox Inc.',
-        unit:'bytes',
-        type: 'freemem',
-        datasourceid: 'freemem',
-        storeType: 'store-json'
-      })
-      
-    ];
-    
-    return Promise.all(proms);
-  })
-  .then(()=>{
-    https.createServer(credentials, app).listen(PORT);
+let loadavg15 = databox.NewDataSourceMetadata();
+loadavg15.Description = 'Databox load average over 15 minutes';
+loadavg15.ContentType = 'application/json';
+loadavg15.Vendor = 'Databox Inc.';
+loadavg15.Unit = '%';
+loadavg15.DataSourceType = 'loadavg15';
+loadavg15.DataSourceID = 'loadavg15';
+loadavg15.StoreType = 'ts';
 
-    monitor.start({ delay: 5000 });
+let freemem = databox.NewDataSourceMetadata();
+freemem.Description = 'Databox free memory in bytes';
+freemem.ContentType = 'application/json';
+freemem.Vendor = 'Databox Inc.';
+freemem.Unit = 'bytes';
+freemem.DataSourceType = 'freemem';
+freemem.DataSourceID = 'freemem';
+freemem.StoreType = 'ts';
 
-    // define handler that will always fire every cycle
-    monitor.on('monitor', function(event) {
-      
-      var loadavg1 = event['loadavg'][0];
-      var loadavg5 = event['loadavg'][1];
-      var loadavg15 = event['loadavg'][2];
-      var freemem = event[freemem];
-      console.log(loadavg1);
+tsc.RegisterDatasource(loadavg1)
+.then(()=>{
+  return tsc.RegisterDatasource(loadavg5);
+})
+.then(()=>{
+  return tsc.RegisterDatasource(loadavg15);
+})
+.then(()=>{
+  return tsc.RegisterDatasource(freemem);
+})
+.catch((err)=>{
+  console.log("Error registering data source:" + err);
+});
 
-      save('loadavg1', event['loadavg'][0]);
-      save('loadavg5', event['loadavg'][1]);
-      save('loadavg15',event['loadavg'][2]);
-      save('freemem', event['freemem']);
 
-      function save(datasourceid,data) {
-        console.log("Saving data::", datasourceid, data);
-        
-        databox.timeseries.write(DATABOX_STORE_BLOB_ENDPOINT, datasourceid, data)
-        .catch((error)=>{
-          console.log("[Error writing to store]", error);
-        });
-      }
+https.createServer(credentials, app).listen(PORT);
 
+monitor.start({ delay: 5000 });
+
+// define handler that will always fire every cycle
+monitor.on('monitor', function(event) {
+
+  var loadavg1 = event['loadavg'][0];
+  var loadavg5 = event['loadavg'][1];
+  var loadavg15 = event['loadavg'][2];
+  var freemem = event[freemem];
+  console.log(loadavg1);
+
+  save('loadavg1', event['loadavg'][0]);
+  save('loadavg5', event['loadavg'][1]);
+  save('loadavg15',event['loadavg'][2]);
+  save('freemem', event['freemem']);
+
+  function save(datasourceid,data) {
+    console.log("Saving data::", datasourceid, {"data":data});
+    json = {"data":data};
+    tsc.Write(datasourceid,json)
+    .then((resp)=>{
+      console.log("Save got response ", resp);
+    })
+    .catch((error)=>{
+      console.log("Error writing to store:", error);
     });
-  })
-  .catch((err)=>{
-    console.log("[ERROR]",err);
-  });
+  }
 
+});
 
-  
 module.exports = app;
